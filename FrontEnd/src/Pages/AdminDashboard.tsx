219dde1566338bc049da49b530e4de967c7aa5c1
Mohamad Reda <dragoongames24@gmail.com>
2025-04-28 15:51:21 +0300

    make the view board for the admin

--- FrontEnd/src/Pages/AdminDashboard.tsx
+++ FrontEnd/src/Pages/AdminDashboard.tsx
@@ -1,26 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faCog, faChartBar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faCog,
+  faChartBar,
+  faCrown,
+} from "@fortawesome/free-solid-svg-icons";
 import AdminCard from "../Components/AdminCard";
+import ViewBoard from "../Components/ViewBoard";
 
 export const AdminDashboard = () => {
   return (
     <div className="max-w-7xl mx-auto grid grid-cols-3">
-      <div className="bg-gray-300 h-screen col-span-1 flex flex-col items-center justify-center border-r border-gray-500">
+      <div className="bg-gray-100 h-screen col-span-1 flex flex-col items-center justify-center border-r border-gray-500">
         <AdminCard
           name="Manage Users"
-          icon={<FontAwesomeIcon className="size-5 mr-2" icon={faUser} />}
+          icon={
+            <FontAwesomeIcon
+              className="size-5 mr-2 text-gray-600"
+              icon={faUser}
+            />
+          }
         />
         <AdminCard
           name="Settings"
-          icon={<FontAwesomeIcon className="size-5 mr-2" icon={faCog} />}
+          icon={
+            <FontAwesomeIcon
+              className="size-5 mr-2 text-gray-600"
+              icon={faCog}
+            />
+          }
         />
         <AdminCard
           name="Reports"
-          icon={<FontAwesomeIcon className="size-5 mr-2" icon={faChartBar} />}
+          icon={
+            <FontAwesomeIcon
+              className="size-5 mr-2 text-gray-600"
+              icon={faChartBar}
+            />
+          }
+        />
+        <AdminCard
+          name="Manage Memeberships"
+          icon={
+            <FontAwesomeIcon
+              className="size-5 mr-2 text-gray-600"
+              icon={faCrown}
+            />
+          }
         />
       </div>
-      <div className="bg-gray-300 h-screen col-span-2 flex items-center justify-center">
-        hh
+      <div className="bg-gray-100 h-screen col-span-2 flex items-center justify-center">
+        <ViewBoard name="Users"></ViewBoard>
       </div>
     </div>
   );
