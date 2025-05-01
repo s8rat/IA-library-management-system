e2231cfaa18b6ff48c79ee7eee57497f2d6bbdbc
Mo_Alsgheer <mohamedalsgher2004@gmail.com>
2025-05-01 14:56:46 +0300

    redesign footer

--- FrontEnd/src/Pages/auth/Layout.tsx
+++ FrontEnd/src/Pages/auth/Layout.tsx
@@ -6,7 +6,7 @@ const AuthLayout = () => {
     return (
         <div className="min-h-screen flex flex-col bg-gray-50">
             <Navigation />
-            <main className="flex-1 flex pt-16 md:pt-20">
+            <main className="flex-1 flex pt-16 md:pt-20 min-h-screen">
                 <Outlet />
             </main>
             <Footer />
