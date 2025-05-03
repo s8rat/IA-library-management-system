52e1310cf4d8b343a075a92e50212965ac2d755f
lolko7ol <Mohammed_20225218@fci.helwan.edu.eg>
2025-05-03 15:08:40 +0300

    git ignore

--- BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e6b8c13911d2c8fa6f1e29981713e292e6a8e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2deb32c363658756825ccf69ccb6ec465f9f2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
